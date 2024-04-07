d2f2309b1a5031d82cf0ec95030f22a5ee2e705c
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-07 10:59:21 -0700

    Keep new migrations from trying to recreate User table

--- db/migrate/20240407005134_add_name_to_users.rb
+++ db/migrate/20240407005134_add_name_to_users.rb
@@ -1,6 +1,8 @@
 class AddNameToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
+    unless table_exists?(:users)
+      add_column :users, :first_name, :string
+      add_column :users, :last_name, :string
+    end
   end
 end
