0f7f67b1c6205ec170dfe1b0cc00035b6fa4daf8
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-07 11:44:14 -0700

    Add conditional for future migrations, if needed

--- db/migrate/20240407152314_add_code_to_airports.rb
+++ db/migrate/20240407152314_add_code_to_airports.rb
@@ -1,5 +1,7 @@
 class AddCodeToAirports < ActiveRecord::Migration[7.1]
   def change
-    add_column :airports, :code, :string
+    # unless table_exists?(:airports)
+      add_column :airports, :code, :string
+    # end
   end
 end
