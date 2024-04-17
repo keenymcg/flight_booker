c9d56521d84c3e3c6c8d15cd6987771fb08ef2b3
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-17 14:53:07 -0700

    Add console print statement

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,6 @@ Airport.create(name: 'Los Angeles International Airport', code: 333, location: '
 Flight.create(flight_number: 'UA 123', departure_date: '2024-04-18', departure_time: '08:00:00', arrival_date: '2024-04-18', arrival_time: '12:00:00', price: 500, departure_airport_id: 1, arrival_airport_id: 2)
 Flight.create(flight_number: 'AA 456', departure_date: '2024-04-18', departure_time: '10:00:00', arrival_date: '2024-04-18', arrival_time: '14:00:00', price: 600, departure_airport_id: 2, arrival_airport_id: 1)
 Flight.create(flight_number: 'DL 789', departure_date: '2024-04-18', departure_time: '12:00:00', arrival_date: '2024-04-18', arrival_time: '16:00:00', price: 700, departure_airport_id: 1, arrival_airport_id: 3)
+
+p "Created #{Airport.count} airports"
+p "Created #{Flight.count} flights"
