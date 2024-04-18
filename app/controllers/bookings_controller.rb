622833c44c292fddcc9cd856b7d02d7aee509c93
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-18 12:09:21 -0700

    Create Bookings model & controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,7 @@
+class BookingsController < ApplicationController
+  def new
+    # @booking = Booking.new
+    # @flight = Flight.find(params[:flight_id])
+    # params[:passenger_count].to_i.times { @booking.passengers.build }
+  end
+end
