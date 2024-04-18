c0216a18a6a0a62ebdb68d928f5af5ed87fa7285
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-18 12:09:39 -0700

    Revamp private query_params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,7 @@ class FlightsController < ApplicationController
     def index
         @flights = Flight.all
         @flight_dates = Flight.pluck(:departure_date).map { |date| date.strftime("%m/%d/%Y") }.uniq
-
+        @passenger_count = query_params[:passenger_count]
         @searched_flights = Flight.search(params[:departure_airport_id], params[:arrival_airport_id], 
         params[:departure_date], params[:passenger_count])
     end
@@ -13,8 +13,11 @@ class FlightsController < ApplicationController
 
     private
 
-    def flight_params
-        params.require(:flight).permit(:flight_number, :departure_time, :arrival_time, :price, :arrival_airport_id, :departure_airport_id)
+    def query_params
+        params.permit(:flight_number, :departure_time, :arrival_time, 
+        :arrival_airport_id, :departure_airport_id, 
+        :departure_date, :arrival_date, :passenger_count,
+        :commit, :authenticity_token)
     end
 
 end
