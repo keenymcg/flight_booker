7a065aa77ad3e0dfa80e056e20df301e3c0a3115
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-18 12:11:09 -0700

    Add routes for bookings & airports

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   
+  # Defines the root path route ("/") to the flights index page
   root 'flights#index'
+
   resources :flights
+  resources :bookings
+  resources :airports
 
 end
