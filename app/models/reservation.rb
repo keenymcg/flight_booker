3c995374b22f463e7d8b607bb17e1dbd35920377
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-18 12:10:10 -0700

    Deprecate Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
+  
+  # THIS WHOLE MODEL MAY BE DEPRECATED / USELESS, USING BOOKING INSTEAD
+  
   belongs_to :user
   belongs_to :flight
   has_many :passengers
