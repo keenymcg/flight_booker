a17d5c5eb96d427c1deba7bdd03fee0acf772131
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-18 14:56:13 -0700

    Create custom data formats for view

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,7 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport', foreign_key: 'departure_airport_id'
   belongs_to :arrival_airport, class_name: 'Airport', foreign_key: 'arrival_airport_id'
-  has_many :reservations
+  # has_many :reservations
+  has_many :bookings
 
   def self.search(departure_airport_id, arrival_airport_id, departure_date, passenger_count) 
     if departure_airport_id.present? && arrival_airport_id.present? && departure_date.present?
@@ -15,6 +16,22 @@ class Flight < ApplicationRecord
 
   def duration
     ((arrival_time - departure_time) / 60).to_i
+  end
+
+  def departure_date_formatted
+    departure_date.strftime('%B %d, %Y')
+  end
+
+  def departure_time_formatted
+    departure_time.strftime('%I:%M %p')
+  end
+
+  def arrival_date_formatted
+    arrival_date.strftime('%B %d, %Y')
+  end
+
+  def arrival_time_formatted
+    arrival_time.strftime('%I:%M %p')
   end
 
 end
