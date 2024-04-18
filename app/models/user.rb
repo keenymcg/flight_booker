514a99bef4edbe168532295a6d507a78d9bdf436
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-18 12:10:22 -0700

    Remove reservations association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # Associations
-  has_many :reservations
+  # # Associations
+  # has_many :reservations # deprecated? Using bookings instead of reservations
   has_many :flights, through: :reservations
+  
 end
