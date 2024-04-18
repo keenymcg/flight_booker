a60b30593ea775b39843331e6c5551d545e42f7b
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-18 12:09:57 -0700

    Deprecate Passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,8 @@
 class Passenger < ApplicationRecord
-  belongs_to :reservation
+  
+  # THIS WHOLE MODEL MAY BE DEPRECATED / USELESS, USING USER INSTEAD
+  
+  belongs_to :reservation # deprecated? Using booking instead of reservation
+  belongs_to :booking
+
 end
