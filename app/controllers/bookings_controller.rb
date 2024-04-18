b334b83586ef40a1ad583e45ab9fda85d9139dc3
KEENAN MCGUCKIN <118635604+keenymcg@users.noreply.github.com>
2024-04-18 14:55:39 -0700

    Add controller actions

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,31 @@
 class BookingsController < ApplicationController
   def new
-    # @booking = Booking.new
-    # @flight = Flight.find(params[:flight_id])
-    # params[:passenger_count].to_i.times { @booking.passengers.build }
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @passenger_count = params[:passenger_count].to_i
+    @booking.passengers.build
   end
+
+  def create
+
+    @flight = Flight.find(booking_params[:flight_id])
+    @booking = @flight.bookings.build(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show 
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :age, :gender])
+  end
+
 end
